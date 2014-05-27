24770661d832889f1a35a334ce05859115f47541
Dāvis <davispuh@gmail.com>
2014-05-28 02:25:07 +0300

    Add more IDCRL constants

--- lib/live_identity/idcrl/constants.rb
+++ lib/live_identity/idcrl/constants.rb
@@ -4,12 +4,19 @@ class LiveIdentity
             MaxLiveIDLength = 113
             MaxLivePasswordLength = 31
 
-            PPCRL_CREDTYPE_MEMBERNAMEONLY      = 'ps:active'
-            PPCRL_CREDTYPE_PASSWORD            = 'ps:password'
-            PPCRL_CREDTYPE_ACTIVE              = 'ps:membernameonly'
-            PPCRL_PROPERTY_FEDERATIONBRANDNAME = 'IsDomainUser'
-            PPCRL_CREDPROPERTY_ISDOMAINUSER    = 'FederationBrandName'
-
+            PPCRL_CREDTYPE_MEMBERNAMEONLY        = 'ps:membernameonly'
+            PPCRL_CREDTYPE_PASSWORD              = 'ps:password'
+            PPCRL_CREDTYPE_ACTIVE                = 'ps:active'
+            PPCRL_CREDTYPE_VIRUTUALAPP           = 'ps:virtualapp'
+            PPCRL_PROPERTY_FEDERATIONBRANDNAME   = 'FederationBrandName'
+            PPCRL_CREDPROPERTY_ISDOMAINUSER      = 'IsDomainUser'
+            PPCRL_CREDPROPERTY_BRANDIDLIST       = 'BrandIDList'
+            PPCRL_CREDPROPERTY_CID               = 'CID'
+            PPCRL_CREDPROPERTY_ISWINLIVEUSER     = 'IsWinLiveUser'
+            PPCRL_CREDPROPERTY_MAINBRANDID       = 'MainBrandID'
+            PPCRL_CREDPROPERTY_MEMBER_NAME       = 'MemberName'
+            PPCRL_CREDPROPERTY_ONETIMECREDENTIAL = 'OneTimeCredential'
+            PPCRL_CREDPROPERTY_PUIDSTR           = 'PUID'
         end
     end
 end
